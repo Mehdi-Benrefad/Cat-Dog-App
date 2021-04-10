2773d96bdfa947a9ee7ab7d93ef484b6cf0e5ff3
Mehdi Benrefad <benrefadmehdi@gmail.com>
2021-04-10 05:31:26 -0700

    Initial Commit

---
+++ Cat-Dog-App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cat-Dog-App
+//
+//  Created by Mehdi Benrefad on 10/04/2021.
+//  Copyright © 2021 Mehdi Benrefad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Cat-Dog-AppTests/Cat_Dog_AppTests.swift
@@ -0,0 +1,34 @@
+//
+//  Cat_Dog_AppTests.swift
+//  Cat-Dog-AppTests
+//
+//  Created by Mehdi Benrefad on 10/04/2021.
+//  Copyright © 2021 Mehdi Benrefad. All rights reserved.
+//
+
+import XCTest
+@testable import Cat_Dog_App
+
+class Cat_Dog_AppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
