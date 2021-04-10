2a2e55cc8374585741cbc1c18e2ec98220d1dfcb
Mehdi Benrefad <benrefadmehdi@gmail.com>
2021-04-10 16:36:46 -0700

    add cats navigation

---
+++ Cat-Dog-App/CatWelcomeViewController.swift
@@ -0,0 +1,34 @@
+//
+//  CatWelcomeViewController.swift
+//  Cat-Dog-App
+//
+//  Created by Mehdi Benrefad on 10/04/2021.
+//  Copyright © 2021 Mehdi Benrefad. All rights reserved.
+//
+
+import UIKit
+
+class CatWelcomeViewController: UIViewController {
+
+    @IBOutlet weak var start: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        start.layer.cornerRadius=25.0
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
